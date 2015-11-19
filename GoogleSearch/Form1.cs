05711a19397f7819f5d3ab51d5cb129c687ab7b3
nickraz <obik.paren@gmail.com>
2015-11-19 12:39:00 +0300

    Создал новый проект

---
+++ GoogleSearch/Form1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+using DevExpress.UserSkins;
+using DevExpress.XtraEditors;
+
+
+namespace WindowsApplication1
+{
+    public partial class Form1 : XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
